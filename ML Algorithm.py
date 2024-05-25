6f7240fb330337bfd379dfaf65a43fd1c95889d0
Ritika Rawat <85759526+Ritikarawat858@users.noreply.github.com>
2024-05-25 17:15:20 +0530

    Update ML Algorithm.py

--- ML Algorithm.py
+++ ML Algorithm.py
@@ -1,12 +1,12 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, GridSearchCV
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.preprocessing import LabelEncoder
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# Load the dataset
+# Load  dataset
 df = pd.read_csv('/Users/apple/Desktop/kepler_data.csv', skiprows=52)  # Skip first 52 rows
 
 df_numeric = df.select_dtypes(include=['int', 'float'])
@@ -16,30 +16,41 @@ df_numeric['koi_disposition'] = label_encoder.fit_transform(df['koi_disposition'
 
 label_mapping = dict(zip(label_encoder.classes_, label_encoder.transform(label_encoder.classes_)))
 
-# Separate features X and target variable y
 X = df_numeric.drop(columns=['koi_disposition'])
 y = df_numeric['koi_disposition']
 
-# Split data into training and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
 
 model = XGBClassifier()
 
-# Train the model
-model.fit(X_train, y_train)
 
-y_pred = model.predict(X_test)
+param_grid = {
+    'n_estimators': [50, 100, 150],
+    'max_depth': [3, 5, 7],
+    'learning_rate': [0.01, 0.1, 0.3],
+    'subsample': [0.5, 0.7, 1.0],
+    'colsample_bytree': [0.5, 0.7, 1.0]
+}
 
-# Evaluate model accuracy
+# Perform grid search with cross-validation
+grid_search = GridSearchCV(estimator=model, param_grid=param_grid, cv=3, scoring='accuracy', verbose=2, n_jobs=-1)
+grid_search.fit(X_train, y_train)
+
+best_params = grid_search.best_params_
+best_model = grid_search.best_estimator_
+
+best_model.fit(X_train, y_train)
+
+y_pred = best_model.predict(X_test)
+
 accuracy = accuracy_score(y_test, y_pred)
 print("Accuracy:", accuracy)
 
-#  classification report
+# Classification report
 print("Classification Report:")
 print(classification_report(y_test, y_pred))
 
-#  confusion matrix
+# Confusion matrix
 cm = confusion_matrix(y_test, y_pred)
 plt.figure(figsize=(8, 6))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, xticklabels=label_mapping.keys(), yticklabels=label_mapping.keys())
@@ -51,3 +62,6 @@ plt.show()
 print("Numerical Label -> Categorical Value Mapping:")
 for numerical_label, categorical_value in label_mapping.items():
     print(f"{numerical_label} -> {categorical_value}")
+
+
+
